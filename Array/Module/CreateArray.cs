86fdea6f3b45850f975dd08003401e61609c6513
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-05-02 15:36:54 +0600

    Add project files.

---
+++ Array/Module/CreateArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array.Module
+{
+    public class CreateArray
+    {
+        public void setArray()
+        {
+            try
+            {
+                int[] arr = new int[5];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = i;
+                }
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[5]);
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Module_5_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Array.Module;
+
+
+/// <summary>
+/// 2.	Написать программу, которая обращается к элементам массива по индексу и выходит за его пределы. После обработки исключения вывести в финальном блоке сообщение о завершении обработки массива.
+/// </summary>
+
+
+namespace Module_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateArray arr = new CreateArray();
+            arr.setArray();
+
+            Console.ReadKey();
+        }
+    }
+}
